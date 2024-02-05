4c3641699969ff7c6fb148d7f1b53c0eb2ad9db9
Arkano Aqilla Tristanoza <tristanozaa@gmail.com>
2024-02-05 17:34:40 +0700

    update

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ios::sync_with_stdio(0);
+cin.tie(0);
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef long long ll;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+int main() {
+    return 0;
+}
