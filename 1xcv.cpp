98e35cd88b9352ce1f1d36c699e331c8f2b91f6d
Arkano Aqilla Tristanoza <tristanozaa@gmail.com>
2024-02-16 22:38:46 +0700

    pemanasan

---
+++ 1xcv.cpp
@@ -0,0 +1,24 @@
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define dbg(v)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#endif
+
+void solve() {}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+
+  solve();
+  return 0;
+}
---
+++ TLX/Courses/Pemrograman Kompetitif Dasar/01. Perkenalan Pemrograman Kompetitif/B. Runtuh/code.cpp
@@ -0,0 +1,77 @@
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = long double; // or double, if TL is tight
+using str = string;     // yay python!
+
+// pairs
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template <class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT > using V = vector<T>;
+tcT, size_t SZ > using AR = array<T, SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vl = V<ll>;
+using vd = V<db>;
+using vs = V<str>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vpd = V<pd>;
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+
+#define lb lower_bound
+#define ub upper_bound
+tcT > int lwb(V<T> &a, const T &b) { return int(lb(all(a), b) - bg(a)); }
+tcT > int upb(V<T> &a, const T &b) { return int(ub(all(a), b) - bg(a)); }
+
+// loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define rep(a) F0R(_, a)
+#define each(a, x) for (auto &a : x)
+
+#ifndef ONLINE_JUDGE
+#define dbg(v)                                                                 \
+  cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#endif
+
+void solve() {}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+#endif
+
+  solve();
+  return 0;
+}
