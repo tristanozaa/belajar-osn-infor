4c1d7bca1e390e34dea515d879828f88e09ffa6c
tristanozaa <tristanozaa@gmail.com>
2024-06-09 09:56:57 +0700

    cover

--- TLX/Courses/Pemrograman Kompetitif Dasar/02. Matematika Diskret Dasar/B. Prima Ke-K/code.cpp
+++ TLX/Courses/Pemrograman Kompetitif Dasar/02. Matematika Diskret Dasar/B. Prima Ke-K/code.cpp
@@ -4,47 +4,10 @@ using namespace std;
 
 #ifndef ONLINE_JUDGE
 #define dbg(v)                                                                 \
-  cerr << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+  cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
 #endif
 
-bool isPrime(int n) {
-  // Corner case
-  if (n <= 1)
-    return false;
-
-  // Check from 2 to n-1
-  for (int i = 2; i <= n / 2; i++)
-    if (n % i == 0)
-      return false;
-
-  return true;
-}
-
-void solve() {
-  bool prima[10000];
-  for (int i = 0; i < 10000; i++) {
-    if (isPrime(i) == true) {
-      prima[i] = true;
-    } else {
-      prima[i] = false;
-    }
-    dbg(prima[i]);
-  }
-  int t;
-  cin >> t;
-  int number, temp, ans;
-  for (int i = 0; i < t; i++) {
-    cin >> number;
-    temp = 0;
-    for (int j = 3; temp != number; j++) {
-      if (prima[j] == true) {
-        temp++;
-        ans = j;
-      }
-    }
-    cout << ans - 2 << "\n";
-  }
-}
+void solve() {}
 
 int main() {
   ios::sync_with_stdio(0);
